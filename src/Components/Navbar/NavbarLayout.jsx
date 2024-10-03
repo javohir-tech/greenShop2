b64a68d2d2d9f1d3c6ef490a4dfaa6eceebbe28c
Suvonov Javohir <suvonovjavohir625@gmail.com>
2024-10-03 19:26:47 +0500

    navbar setting

--- src/Components/Navbar/NavbarLayout.jsx
+++ src/Components/Navbar/NavbarLayout.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
